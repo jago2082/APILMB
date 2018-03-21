d549d369f1c27e3e9d66261475ea429b7e2c5e5b
Javier <Javier@MOVILES2>
2018-03-21 14:32:09 -0500

    Agregar archivos de proyecto.

---
+++ APILMB/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace APILMB.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ APILMB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="APILMB.WebApiApplication" Language="C#" %>
